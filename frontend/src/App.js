b1550580a31af387a514feeed08bf73f6279a77f
Tiago Gomes <tidgomes@gmail.com>
2023-04-11 11:23:04 -0300

    set the events routes as children of the EventsRootLayout page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,3 @@
-// Challenge / Exercise
-
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import HomePage from './pages/Home'
 import EventsPage from './pages/Events'
@@ -9,6 +5,7 @@ import EventDetailPage from './pages/EventDetail'
 import NewEventPage from './pages/NewEvent'
 import EditEventPage from './pages/EditEvent'
 import RootLayout from './pages/Root'
+import EventsRootLayout from './pages/EventsRoot'
 
 const router = createBrowserRouter([
   {
@@ -16,10 +13,16 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: 'events', element: <EventsPage /> },
-      { path: 'events/:eventId', element: <EventDetailPage /> },
-      { path: 'events/new', element: <NewEventPage /> },
-      { path: 'events/:eventId/edit', element: <EditEventPage /> }
+      {
+        path: 'events',
+        element: <EventsRootLayout />,
+        children: [
+          { path: '', element: <EventsPage /> },
+          { path: ':eventId', element: <EventDetailPage /> },
+          { path: 'new', element: <NewEventPage /> },
+          { path: ':eventId/edit', element: <EditEventPage /> }
+        ]
+      }
     ]
   }
 ])
