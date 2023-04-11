f744b34eb5a350b05cae159c2ccb5725beacf881
Tiago Gomes <tidgomes@gmail.com>
2023-04-11 11:13:49 -0300

    render the link to the EventDetails in the Events list

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom'
+
+const DUMMY_EVENTS = [
+  { id: 'e1', title: 'some event' },
+  { id: 'e2', title: 'some event' },
+  { id: 'e3', title: 'some event' }
+]
+
 const EventsPage = () => {
-  return <h1>Events</h1>
+  return (
+    <>
+      <h1>Events</h1>
+      <ul>
+        {DUMMY_EVENTS.map(event => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 export default EventsPage
