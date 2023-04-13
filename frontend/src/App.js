7a736410a5377b57b8d13d7f895d5e756131e29c
Tiago Gomes <tidgomes@gmail.com>
2023-04-13 17:00:39 -0300

    enable editting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,12 @@ import EventDetailPage, {
   loader as eventDetailsLoader,
   action as deleteEvent
 } from './pages/EventDetail'
-import NewEventPage, { action as newEventAction } from './pages/NewEvent'
+import NewEventPage from './pages/NewEvent'
 import EditEventPage from './pages/EditEvent'
 import RootLayout from './pages/Root'
 import EventsRootLayout from './pages/EventsRoot'
 import ErrorPage from './pages/Error'
+import { action as manipulateAction } from './components/EventForm'
 
 const router = createBrowserRouter([
   {
@@ -37,10 +38,14 @@ const router = createBrowserRouter([
                 element: <EventDetailPage />,
                 action: deleteEvent
               },
-              { path: 'edit', element: <EditEventPage /> }
+              {
+                path: 'edit',
+                element: <EditEventPage />,
+                action: manipulateAction
+              }
             ]
           },
-          { path: 'new', element: <NewEventPage />, action: newEventAction }
+          { path: 'new', element: <NewEventPage />, action: manipulateAction }
         ]
       }
     ]
--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,4 +1,10 @@
-import { Form, useNavigation, useActionData } from 'react-router-dom'
+import {
+  Form,
+  useNavigation,
+  useActionData,
+  json,
+  redirect
+} from 'react-router-dom'
 
 import classes from './EventForm.module.css'
 
@@ -13,7 +19,7 @@ function EventForm({ method, event }) {
   }
 
   return (
-    <Form method='POST' className={classes.form}>
+    <Form method={method} className={classes.form}>
       {data && data.errors && (
         <ul>
           {Object.values(data.errors).map(err => (
@@ -74,3 +80,38 @@ function EventForm({ method, event }) {
 }
 
 export default EventForm
+
+export const action = async ({ request, params }) => {
+  const method = request.method
+  const data = await request.formData()
+
+  const eventData = {
+    title: data.get('title'),
+    image: data.get('image'),
+    date: data.get('date'),
+    description: data.get('description')
+  }
+
+  let url = 'http://localhost:8080/events'
+
+  if (method === 'PATCH') {
+    const id = params.eventId
+
+    url = `http://localhost:8080/events/${id}`
+  }
+
+  const response = await fetch(url, {
+    method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(eventData)
+  })
+
+  if (response.status === 422) return response
+
+  if (!response.ok)
+    throw json({ message: 'Could not save event' }, { status: 500 })
+
+  return redirect('/events')
+}
